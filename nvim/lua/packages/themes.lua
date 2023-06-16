3009a7aac4944028c5afb291f0bd9349d611a4ee
Elijah <elijahshellsanchez@outlook.com>
2023-06-16 11:11:55 +0200

    Made background transparent for fun

--- nvim/lua/packages/themes.lua
+++ nvim/lua/packages/themes.lua
@@ -5,5 +5,7 @@ return {
     priority = 1000,
     config = function()
         vim.cmd([[colorscheme catppuccin]])
+        vim.cmd([[highlight Normal guibg=none]])
+        vim.cmd([[highlight NonText guibg=none]])
     end,
 }
